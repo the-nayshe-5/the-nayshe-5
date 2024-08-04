507548a0114443511948325c02fddc2a9ed87379
Naysah Sheikh <135449013+the-nayshe-5@users.noreply.github.com>
2024-08-04 15:53:01 +0530

    Update machine_learning.js

--- machine_learning.js
+++ machine_learning.js
@@ -1,5 +1,5 @@
 cards = document.querySelectorAll('.card');
-urls = ['./img/jpmc-dashboard.png', './img/air-quality.png' './img/bcg-dashboard.png', './img/house-price.png',]
+urls = ['./img/jpmc-dashboard.png', './img/air-quality.png', './img/bcg-dashboard.png', './img/house-price.png',]
 
 for (let i = 0; i < cards.length; i++) {
     cards[i].style.backgroundImage = `url(${urls[i]})`;
