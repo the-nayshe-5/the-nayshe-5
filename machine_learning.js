7c8033efe25ca8848654137243b1ec33095251bf
Naysah Sheikh <135449013+the-nayshe-5@users.noreply.github.com>
2024-08-04 14:47:36 +0530

    Create machine_learning.js

---
+++ machine_learning.js
@@ -0,0 +1,6 @@
+cards = document.querySelectorAll('.card');
+urls = ['./img/jpmc-dashboard.png', './img/air-quality.png' './img/bcg-dashboard.png', './img/house-price.png',]
+
+for (let i = 0; i < cards.length; i++) {
+    cards[i].style.backgroundImage = `url(${urls[i]})`;
+}
