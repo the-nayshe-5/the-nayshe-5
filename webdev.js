f11d1a5fd85d2cf9affdc90623283eea23628731
Naysah Sheikh <135449013+the-nayshe-5@users.noreply.github.com>
2024-08-22 19:05:12 +0530

    Update webdev.js

--- webdev.js
+++ webdev.js
@@ -1,5 +1,5 @@
 cards = document.querySelectorAll('.card');
-urls = ['./img/drofin.png', './img/taskops.png',]
+urls = ['./img/drofin.png', './img/taskops.png', './img/marvelquiz.png']
 
 for (let i = 0; i < cards.length; i++) {
     cards[i].style.backgroundImage = `url(${urls[i]})`;
